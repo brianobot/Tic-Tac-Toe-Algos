03ba672e13ec347da7a065955f7bd4937370f14d
Brian Obot <brianobot9@gmail.com>
2024-02-02 18:11:46 +0100

    Refactor the tournament module and move certain functionality into a utils module

--- tournament.py
+++ tournament.py
@@ -4,16 +4,15 @@ After Implementing an algorithm for the computer player
 
 import environ
 import players
-import importlib
 
 from app import TicTacToe
+from utils import load_players
 from collections import Counter
 
 env = environ.Env()
 environ.Env.read_env()
 
 ITERATIONS = env("ITERATIONS", default=500)
-PLAYER_MODULE = importlib.import_module("players")
 
 # list of default dummy players for reference, if you algorithm loses to this players
 players_list = [
@@ -23,44 +22,30 @@ players_list = [
     players.MiddleSpotPickerPlayer,
 ]
 
-with open("players_list.txt") as player_data:
-    for player in player_data.readlines():
-        player: str
-        if player.isspace():
-            continue
-        try:
-            player = getattr(PLAYER_MODULE, player)
-        except AttributeError as err:
-            print(f"Error while adding player: {player}")
-            continue
-        else:
-            players_list.append(player)
-            print(f"Successfully Added player: {player}")
 
-
+players_list += load_players()
 input("sdfsdfsdf")
 
 league_wins = []
 
 
 def compare_players(a, b, iterations=ITERATIONS):
+    """
+    Run an Nth number of tic tac toe games between two players and record the win ratio.
+    """
     results = []
     for run in range(iterations):
         game = TicTacToe(a, b)
         while game.check_for_winner() == -1:
-            # print_board(game)
             if game.computer_player and (game.player == game.computer_player.id):
                 r, c = game.computer_player.make_move()
-                # print(f"Computer player {game.computer_player.id} plays {r, c}")
                 game.make_move(r, c)
             elif game.computer_player_2 and (game.player == game.computer_player_2.id):
                 r, c = game.computer_player.make_move()
-                # print(f"Computer player {game.computer_player_2.id} plays {r, c}")
                 game.make_move(r, c)
             else:
                 pass
 
-        # print_board(game)
         x = game.check_for_winner()
         results.append(x)
 
---
+++ utils.py
@@ -0,0 +1,25 @@
+import importlib
+
+PLAYER_MODULE = importlib.import_module("players")
+
+
+def load_players() -> list:
+    """
+    Read and Load Players classes from the players_list.txt file
+    invalid players are indicated, and are not added to the returned list
+    """
+    players = []
+    with open("players_list.txt") as players_data:
+        for player in players_data.readlines():
+            player: str
+            if player.isspace():
+                continue
+            try:
+                player_class = getattr(PLAYER_MODULE, player)
+            except AttributeError as err:
+                print(f"Error while adding player: {player}")
+                continue
+            else:
+                players.append(player_class)
+                print(f"Successfully Added player: {player}")
+    return players
